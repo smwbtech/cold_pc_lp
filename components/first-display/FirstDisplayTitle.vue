23dc907a31c409a9c8dfe373b9964179697c4b6b
smwbtech <smwb.tech@gmail.com>
2019-10-31 18:03:57 +0300

    Create FirstDisplayTitle.vue

---
+++ components/first-display/FirstDisplayTitle.vue
@@ -0,0 +1,139 @@
+<template>
+	<div class="title-block">
+		<transition type="transition" name="header">
+			<h1 v-show="isVisible">
+				Аппаратные решения для экстримально низких температур
+			</h1>
+		</transition>
+		<transition-group
+			tag="ul"
+			:class="[showList ? 'visible' : '']"
+			:css="false"
+			appear
+			@enter="enter"
+		>
+			<li
+				v-for="(text, index) in applicationsTitle"
+				:key="`${index}-title`"
+				:data-index="index"
+			>
+				<span>{{ text }}</span>
+			</li>
+		</transition-group>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		isVisible: {
+			type: Boolean,
+			default: false
+		},
+		showList: {
+			type: Boolean,
+			default: false
+		}
+	},
+
+	data() {
+		return {
+			applicationsTitle: [
+				'система видеонаблюдения за нефтепроводом',
+				'система контроля и управления доступом',
+				'система охранного освещения'
+			]
+		};
+	},
+
+	methods: {
+		enter(el, done) {
+			const delay = (+el.dataset.index + 1) * 400;
+			setTimeout(() => {
+				el.classList.add('active');
+				done();
+			}, delay);
+		}
+	}
+};
+</script>
+
+<style scoped>
+/* block with title and applications list */
+.title-block {
+	position: relative;
+	z-index: 20;
+	display: flex;
+	flex-flow: column;
+	justify-content: center;
+	width: calc(var(--column) * 9 + var(--gutter) * 8);
+	margin-left: calc(var(--column) * 2 + var(--gutter) * 2);
+	text-align: left;
+	color: #fff;
+
+	& h1 {
+		font-family: 'Intro', sans-serif;
+	}
+
+	& ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		font-weight: 500;
+		font-size: 1.2em;
+		opacity: 0;
+
+		&.visible {
+			opacity: 1;
+		}
+
+		& li {
+			position: relative;
+			margin-bottom: var(--gutter);
+			opacity: 0;
+			transform: trnaslateX(-20px);
+			transition: opacity 0.3s ease-in, transform 0.3s ease-out;
+
+			& span {
+				position: relative;
+				z-index: 10;
+			}
+
+			&:before {
+				content: '';
+				display: block;
+				position: absolute;
+				z-index: 9;
+				height: 100%;
+				width: 15px;
+				background-color: var(--orange);
+				transform-origin: center;
+				transform: rotate(0deg);
+				transition: transform 0.3s ease-in;
+			}
+
+			&.active {
+				opacity: 1;
+				transform: trnaslateX(0px);
+
+				&:before {
+					transform: rotate(45deg);
+				}
+			}
+		}
+	}
+} /* end - block with title and applications list */
+
+/* Animations and transitions */
+
+/* transitions for h1 */
+.header-enter {
+	transform: translateY(-20px);
+	opacity: 0;
+}
+
+.header-enter-active {
+	transition: opacity 0.3s ease-in, transform 0.3s ease-out;
+}
+/* end - transitions for h1 */
+</style>
