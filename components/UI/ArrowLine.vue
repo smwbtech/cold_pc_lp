0c35b791e1c0ba24348ef90b624ae14136e4d1fd
smwbtech <smwb.tech@gmail.com>
2019-10-31 12:05:59 +0300

    Create ArrowLine.vue
    Component for decorative line with arrow, which shows direction

---
+++ components/UI/ArrowLine.vue
@@ -0,0 +1,82 @@
+<template>
+	<div
+		:class="['line', this.xAxis, this.isVisible ? 'visible' : '']"
+		:style="styleObj"
+	/>
+</template>
+
+<script>
+export default {
+	props: {
+		isVisible: {
+			type: Boolean,
+			default: false
+		},
+		backgroudColor: {
+			type: String,
+			default: '#fff'
+		},
+		xAxis: {
+			type: String,
+			default: 'left'
+		}
+	},
+
+	computed: {
+		styleObj() {
+			return {
+				backgroundColor: this.backgroudColor
+			};
+		}
+	}
+};
+</script>
+
+<style scoped>
+.line {
+	position: absolute;
+	top: var(--row);
+	z-index: 5;
+	height: calc(100vh - (var(--row) * 2));
+	width: 5px;
+	transform-origin: top center;
+	transform: scaleY(0);
+	transition: transform 0.5s ease-in;
+
+	&.left {
+		left: var(--column);
+	}
+
+	&.right {
+		right: var(--column);
+	}
+
+	&:before,
+	&:after {
+		content: '';
+		display: block;
+		position: absolute;
+		width: 5px;
+		height: 40px;
+		background-color: inherit;
+		bottom: -40px;
+		transform-origin: bottom center;
+		transform: rotate(0deg);
+		transition: transform 0.3s ease-out;
+		transition-delay: 0.6s;
+	}
+
+	/* modificator for visible line */
+	&.visible {
+		transform: scaleY(1);
+
+		&:before {
+			transform: rotate(-45deg) translateX(1px);
+		}
+
+		&:after {
+			transform: rotate(45deg) translateX(-1px);
+		}
+	}
+}
+</style>
