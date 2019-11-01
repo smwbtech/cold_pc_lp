09b3da02704394df0bc84f8fd53045346fbeeb24
smwbtech <smwb.tech@gmail.com>
2019-11-01 16:59:25 +0300

    Create ApplicationDisplay.vue

---
+++ components/application-display/ApplicationDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+	<section class="application"></section>
+</template>
+
+<script>
+import IntersectionMixin from '@/assets/js/mixins/IntersectionMixin.js';
+
+export default {
+	mixins: [IntersectionMixin],
+	data() {
+		return {
+			sectionName: 'Решения',
+			isVisible: false,
+			intersectionOptions: {
+				threshold: 0.3
+			}
+		};
+	}
+};
+</script>
+
+<style scoped>
+.application {
+	width: 100%;
+	min-height: 100vh;
+	background-color: var(--grey);
+}
+</style>
