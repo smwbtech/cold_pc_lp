8468d3d6e12abe552d53e6ebcfd7700deede1812
smwbtech <smwb.tech@gmail.com>
2019-11-01 12:00:21 +0300

    Update FirstDisplay.vue
    * Changed show content on 'background-loaded' event

--- components/first-display/FirstDisplay.vue
+++ components/first-display/FirstDisplay.vue
@@ -4,7 +4,7 @@
 		<FirstDisplayTitle :is-visible="isVisible" :show-list="showList" />
 		<FirstDisplayDescription :show-description="showDescription" />
 		<!-- bacground with animated clouds -->
-		<FirstDisplayBackground />
+		<FirstDisplayBackground @backround-loaded="backgeoundLoadedHandler" />
 		<!-- end - bacground with animated clouds -->
 	</section>
 </template>
@@ -32,16 +32,16 @@ export default {
 		};
 	},
 
-	mounted() {
-		setTimeout(() => (this.showLine = true), 300);
-		setTimeout(() => (this.showList = true), 600);
-		setTimeout(() => (this.showDescription = true), 1500);
-	},
-
 	methods: {
 		visibilityChanged(isVisible, entry) {
 			this.isVisible = isVisible;
 			console.log(entry);
+		},
+
+		backgeoundLoadedHandler() {
+			setTimeout(() => (this.showLine = true), 300);
+			setTimeout(() => (this.showList = true), 600);
+			setTimeout(() => (this.showDescription = true), 1500);
 		}
 	}
 };
@@ -62,73 +62,7 @@ export default {
 		height: calc(var(--row) + var(--gutter));
 		min-height: 85px;
 	}
-
-	/* block with title and applications list */
-	& .title-block {
-		position: relative;
-		z-index: 20;
-		display: flex;
-		flex-flow: column;
-		justify-content: center;
-		width: calc(var(--column) * 9 + var(--gutter) * 8);
-		margin-left: calc(var(--column) * 2 + var(--gutter) * 2);
-		text-align: left;
-		color: #fff;
-
-		& h1 {
-			font-family: 'Intro', sans-serif;
-		}
-
-		& ul {
-			list-style: none;
-			margin: 0;
-			padding: 0;
-			font-weight: 500;
-			font-size: 1.2em;
-			opacity: 0;
-
-			&.visible {
-				opacity: 1;
-			}
-
-			& li {
-				position: relative;
-				margin-bottom: var(--gutter);
-				transition: opacity 0.3s ease-in;
-
-				& span {
-					position: relative;
-					z-index: 10;
-				}
-
-				&:before {
-					content: '';
-					display: block;
-					position: absolute;
-					z-index: 9;
-					height: 100%;
-					width: 15px;
-					background-color: var(--orange);
-					transform-origin: center;
-					transform: rotate(45deg);
-				}
-			}
-		}
-	} /* end - block with title and applications list */
 }
-
-/* Animations and transitions */
-
-/* transitions for h1 */
-.header-enter {
-	transform: translateY(-20px);
-	opacity: 0;
-}
-
-.header-enter-active {
-	transition: opacity 0.3s ease-in, transform 0.3s ease-out;
-}
-/* end - transitions for h1 */
 
 /* transition for description block */
 </style>
