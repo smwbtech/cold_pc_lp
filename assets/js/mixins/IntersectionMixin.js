f07db0451cf8ba53db2dadf1ae5e80c41fcf2bfb
smwbtech <smwb.tech@gmail.com>
2019-11-01 16:59:11 +0300

    Create IntersectionMixin.js

---
+++ assets/js/mixins/IntersectionMixin.js
@@ -0,0 +1,20 @@
+export default {
+	mounted() {
+		const observer = new IntersectionObserver(
+			this.visibilityObserver,
+			this.intersectionOptions
+		);
+		observer.observe(this.$el);
+	},
+
+	methods: {
+		visibilityObserver(entries, observer) {
+			entries.forEach((entry) => {
+				this.isVisible = entry.isIntersecting;
+				if (entry.isIntersecting) {
+					console.log(this.sectionName);
+				}
+			});
+		}
+	}
+};
