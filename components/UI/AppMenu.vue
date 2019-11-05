c38e4f9e308ac09828359d272183336a3e21b520
smwbtech <smwb.tech@gmail.com>
2019-11-05 12:10:29 +0300

    Update AppMenu.vue
    * Added menu styles for fixed position (used when user scroll down from frirst display)

--- components/UI/AppMenu.vue
+++ components/UI/AppMenu.vue
@@ -3,7 +3,11 @@
 		<nav>
 			<a href="https://nnz-ipc.ru/" target="_blank">
 				<img
-					src="/img/nnz_logo.svg"
+					:src="
+						isFixed
+							? '/img/nnz_logo_colored.svg'
+							: '/img/nnz_logo.svg'
+					"
 					alt="Логотип Ниеншанц-Автоматика"
 				/>
 			</a>
@@ -35,9 +39,61 @@ header {
 		calc(var(--side-padding) + var(--column) * 2 + var(--gutter) * 2);
 	font-family: 'Intro', sans-serif;
 
+	& nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		& img {
+			width: calc(var(--column) * 3 + var(--gutter) * 2);
+		}
+
+		& ul {
+			list-style: none;
+			display: flex;
+			align-items: flex-end;
+			justify-content: center;
+
+			& li {
+				margin-left: calc(var(--column) + var(--gutter) * 2);
+
+				& a {
+					text-decoration: none;
+					color: #fff;
+					transition: color 0.3s ease-in;
+
+					&:hover {
+						color: var(--orange);
+					}
+				}
+			}
+		}
+	}
+
 	&.fixed {
 		position: fixed;
 		background-color: #fff;
+		animation: menu 0.3s ease-out forwards;
+		box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.2);
+
+		& ul {
+			& li {
+				& a {
+					color: var(--dark-blue);
+				}
+			}
+		}
+	}
+}
+
+/* Transitions and animations */
+
+@keyframes menu {
+	0% {
+		transform: translateY(-100%);
+	}
+	100% {
+		transform: translateY(0%);
 	}
 }
 </style>
