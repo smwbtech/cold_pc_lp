73438277ef2fc46bec5e444219932c8f7a9c9295
smwbtech <smwb.tech@gmail.com>
2019-11-05 18:14:14 +0300

    Update ApplicationDisplay.vue
    * Added ApplicationSlider componet
    * Added ArrowLine component
    * Changed styles for component

--- components/application-display/ApplicationDisplay.vue
+++ components/application-display/ApplicationDisplay.vue
@@ -1,11 +1,24 @@
 <template>
-	<section class="application"></section>
+	<section class="application">
+		<ApplicationSlider />
+		<ArrowLine
+			:x-axis="'right'"
+			:backgroud-color="'#083358'"
+			:is-visible="isVisible"
+		/>
+	</section>
 </template>
 
 <script>
+import ApplicationSlider from './ApplicationDisplaySlider.vue';
 import IntersectionMixin from '@/assets/js/mixins/IntersectionMixin.js';
+import ArrowLine from '@/components/UI/ArrowLine.vue';
 
 export default {
+	components: {
+		ArrowLine,
+		ApplicationSlider
+	},
 	mixins: [IntersectionMixin],
 	data() {
 		return {
@@ -21,6 +34,10 @@ export default {
 
 <style scoped>
 .application {
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	width: 100%;
 	min-height: 100vh;
 	background-color: var(--grey);
