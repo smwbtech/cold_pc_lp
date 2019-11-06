ad82db4c14945576e1eca6795ab2bb035a135b73
smwbtech <smwb.tech@gmail.com>
2019-11-06 17:45:53 +0300

    Create ModelsDisplay.vue

---
+++ components/models-display/ModelsDisplay.vue
@@ -0,0 +1,77 @@
+<template>
+	<section class="models">
+		<ArrowLine :is-visible="isVisible" />
+		<div class="models-list">
+			<ModelItem
+				v-for="model in models"
+				:key="model.id"
+				:model-id="model.id"
+				:model-preview="model.previewPhoto.value"
+				:model-name="model.name"
+			/>
+		</div>
+	</section>
+</template>
+<script>
+import ModelItem from './ModelItem.vue';
+import ArrowLine from '@/components/UI/ArrowLine.vue';
+import IntersectionMixin from '@/assets/js/mixins/IntersectionMixin.js';
+export default {
+	components: {
+		ArrowLine,
+		ModelItem
+	},
+	mixins: [IntersectionMixin],
+	data() {
+		return {
+			sectionName: 'модели',
+			isVisible: false,
+			intersectionOptions: {
+				threshold: 0.3
+			}
+		};
+	},
+	computed: {
+		models() {
+			return this.$store.state.models;
+		}
+	},
+	mounted() {
+		console.log(this.$store.state.models);
+	}
+};
+</script>
+<style scoped>
+.models {
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	min-height: 100vh;
+	background-image: url('/img/models-display/bg.jpg');
+	background-repeat: no-repeat;
+	background-size: cover;
+
+	&:before {
+		position: absolute;
+		z-index: 3;
+		top: 0;
+		left: 0;
+		content: '';
+		display: block;
+		width: 100%;
+		height: 10vh;
+		background-image: url('/img/models-display/decore.png');
+		background-size: cover;
+	}
+
+	& .models-list {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		width: calc(var(--column) * 20 + var(--gutter) * 19);
+	}
+}
+</style>
