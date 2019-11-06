301c4b116db446688afeb8171664ed5386501cd1
smwbtech <smwb.tech@gmail.com>
2019-11-06 17:47:54 +0300

    Update index.js
    * Added state.models {Array} that contains models object
    * Added mutation.setModels to fullfill models array

--- store/index.js
+++ store/index.js
@@ -1,11 +1,15 @@
 export const state = () => {
 	return {
-		currentSection: 'Главный экран'
+		currentSection: 'Главный экран',
+		models: []
 	};
 };
 
 export const mutations = {
 	setCurrentSection(state, payload) {
 		state.currentSection = payload;
+	},
+	setModels(state, payload) {
+		state.models.push(...payload);
 	}
 };
