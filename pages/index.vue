be5cb705e0042a2cb9ccdc6c5ba4f9dc41549adb
smwbtech <smwb.tech@gmail.com>
2019-11-06 17:46:47 +0300

    Update index.vue
    * Added fetch method to fullfill vuex store

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,29 @@
 	<div class="container">
 		<FirstDisplay />
 		<ApplicationDisplay />
+		<ModelsDisplay />
 	</div>
 </template>
 
 <script>
 import FirstDisplay from '@/components/first-display/FirstDisplay.vue';
 import ApplicationDisplay from '@/components/application-display/ApplicationDisplay.vue';
+import ModelsDisplay from '@/components/models-display/ModelsDisplay.vue';
+
+import { getToken } from '@/assets/js/api/getToken.js';
+import { getItems } from '@/assets/js/api/getItems.js';
 
 export default {
 	components: {
 		FirstDisplay,
-		ApplicationDisplay
+		ApplicationDisplay,
+		ModelsDisplay
+	},
+
+	async fetch({ store }) {
+		const token = await getToken();
+		const items = await getItems(token);
+		store.commit('setModels', items);
 	}
 };
 </script>
