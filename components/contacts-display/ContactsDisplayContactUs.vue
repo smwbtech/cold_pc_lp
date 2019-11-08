5da40444c2299a899f2c2effa9d2a34b3dd8f07f
smwbtech <smwb.tech@gmail.com>
2019-11-08 11:14:16 +0300

    Update ContactsDisplayContactUs.vue
    * Added transition for component when it intersect viewport

--- components/contacts-display/ContactsDisplayContactUs.vue
+++ components/contacts-display/ContactsDisplayContactUs.vue
@@ -2,25 +2,30 @@
 	<div class="contact-us">
 		<h2>Свяжитесь с нами!</h2>
 		<div class="product">
-			<img
-				src="/img/contacts-display/avatar.png"
-				alt="Олег Волков"
-				class="product__avatar"
-			/>
-			<div class="info">
-				<span class="name"><span>Олег Волков</span></span>
-				<span class="position"
-					>продакт-менеджер компании <br />ООО
-					"Ниеншанц-Автоматика"<br />
-					по продукции IEI</span
-				>
-				<a href="mailto:o.volkov@nnz.ru" class="email"
-					>o.volkov@nnz.ru</a
-				>
-				<a href="tel:+78123265924#5094" class="phone"
-					>8 (812) 326-59-24 # 5094</a
-				>
-			</div>
+			<transition name="avatar">
+				<img
+					v-show="visible"
+					src="/img/contacts-display/avatar.png"
+					alt="Олег Волков"
+					class="product__avatar"
+				/>
+			</transition>
+			<transition name="info">
+				<div v-show="visible" class="info">
+					<span class="name"><span>Олег Волков</span></span>
+					<span class="position"
+						>продакт-менеджер компании <br />ООО
+						"Ниеншанц-Автоматика"<br />
+						по продукции IEI</span
+					>
+					<a href="mailto:o.volkov@nnz.ru" class="email"
+						>o.volkov@nnz.ru</a
+					>
+					<a href="tel:+78123265924#5094" class="phone"
+						>8 (812) 326-59-24 # 5094</a
+					>
+				</div>
+			</transition>
 		</div>
 		<div class="logos">
 			<a href="https://nnz-ipc.ru/" target="_blank"
@@ -33,7 +38,26 @@
 	</div>
 </template>
 <script>
-export default {};
+export default {
+	props: {
+		show: {
+			type: Boolean,
+			default: false
+		}
+	},
+
+	data() {
+		return {
+			visible: false
+		};
+	},
+
+	watch: {
+		show(val, oldVal) {
+			if (val) this.visible = val;
+		}
+	}
+};
 </script>
 <style scoped>
 .contact-us {
@@ -112,5 +136,25 @@ export default {};
 			justify-content: flex-end;
 		}
 	}
+}
+
+/* Transitions and animations */
+.avatar-enter {
+	opacity: 0;
+	transform: translateX(-20px);
+}
+
+.avatar-enter-active {
+	transition: opacity 0.3s ease-in, transform 0.3s ease-out;
+}
+
+.info-enter {
+	opacity: 0;
+	transform: translateX(20px);
+}
+
+.info-enter-active {
+	transition: opacity 0.3s ease-in, transform 0.3s ease-out;
+	transition-delay: 0.3s;
 }
 </style>
