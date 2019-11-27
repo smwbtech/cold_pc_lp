b39bf2ef3b66fb6ba9959d8c2abd0e5061ce73f0
smwbtech <smwb.tech@gmail.com>
2019-11-27 15:21:54 +0300

    Added nuxt-start
    Fixed issue with started script
    
    https://github.com/nuxt/nuxt.js/issues/5424

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'nuxt',
-			script: './node_modules/nuxt/bin/nuxt-start',
+			script: './node_modules/nuxt-start/bin/nuxt-start.js',
 			env: {
 				HOST: '0.0.0.0',
 				PORT: 3007,
